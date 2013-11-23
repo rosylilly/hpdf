62f693acfbf1818193bba99b6d75014a6ae272e4
Sho Kusano <rosylilly@aduca.org>
2013-11-24 04:23:28 +0900

    Implement and tests destination features

--- destination.go
+++ destination.go
@@ -10,3 +10,57 @@ type Destination struct {
 	destination C.HPDF_Destination
 	page        *Page
 }
+
+func (destination *Destination) SetXYZ(left, top, zoom float32) error {
+	C.HPDF_Destination_SetXYZ(
+		destination.destination,
+		C.HPDF_REAL(left), C.HPDF_REAL(top), C.HPDF_REAL(zoom),
+	)
+
+	return destination.page.pdf.GetLastError()
+}
+
+func (destination *Destination) SetFit() error {
+	C.HPDF_Destination_SetFit(destination.destination)
+
+	return destination.page.pdf.GetLastError()
+}
+
+func (destination *Destination) SetFitH(top float32) error {
+	C.HPDF_Destination_SetFitH(destination.destination, C.HPDF_REAL(top))
+
+	return destination.page.pdf.GetLastError()
+}
+
+func (destination *Destination) SetFitV(left float32) error {
+	C.HPDF_Destination_SetFitV(destination.destination, C.HPDF_REAL(left))
+
+	return destination.page.pdf.GetLastError()
+}
+
+func (destination *Destination) SetFitR(left, bottom, right, top float32) error {
+	C.HPDF_Destination_SetFitR(
+		destination.destination,
+		C.HPDF_REAL(left), C.HPDF_REAL(bottom), C.HPDF_REAL(right), C.HPDF_REAL(top),
+	)
+
+	return destination.page.pdf.GetLastError()
+}
+
+func (destination *Destination) SetFitB() error {
+	C.HPDF_Destination_SetFitB(destination.destination)
+
+	return destination.page.pdf.GetLastError()
+}
+
+func (destination *Destination) SetFitBH(top float32) error {
+	C.HPDF_Destination_SetFitBH(destination.destination, C.HPDF_REAL(top))
+
+	return destination.page.pdf.GetLastError()
+}
+
+func (destination *Destination) SetFitBV(left float32) error {
+	C.HPDF_Destination_SetFitBV(destination.destination, C.HPDF_REAL(left))
+
+	return destination.page.pdf.GetLastError()
+}
---
+++ destination_test.go
@@ -0,0 +1,117 @@
+package hpdf
+
+import (
+	. "testing"
+)
+
+func TestSetXYZ(t *T) {
+	pdf, _ := New()
+	page, _ := pdf.AddPage()
+
+	var d *Destination
+
+	d, _ = page.CreateDestination()
+	err := d.SetXYZ(0, 0, 1.00)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestSetFit(t *T) {
+	pdf, _ := New()
+	page, _ := pdf.AddPage()
+
+	var d *Destination
+
+	d, _ = page.CreateDestination()
+	err := d.SetFit()
+
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestSetFitH(t *T) {
+	pdf, _ := New()
+	page, _ := pdf.AddPage()
+
+	var d *Destination
+
+	d, _ = page.CreateDestination()
+	err := d.SetFitH(0)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestSetFitV(t *T) {
+	pdf, _ := New()
+	page, _ := pdf.AddPage()
+
+	var d *Destination
+
+	d, _ = page.CreateDestination()
+	err := d.SetFitV(0)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestSetFitR(t *T) {
+	pdf, _ := New()
+	page, _ := pdf.AddPage()
+
+	var d *Destination
+
+	d, _ = page.CreateDestination()
+	err := d.SetFitR(0, 0, 0, 0)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestSetFitB(t *T) {
+	pdf, _ := New()
+	page, _ := pdf.AddPage()
+
+	var d *Destination
+
+	d, _ = page.CreateDestination()
+	err := d.SetFitB()
+
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestSetFitBH(t *T) {
+	pdf, _ := New()
+	page, _ := pdf.AddPage()
+
+	var d *Destination
+
+	d, _ = page.CreateDestination()
+	err := d.SetFitBH(0)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestSetFitBV(t *T) {
+	pdf, _ := New()
+	page, _ := pdf.AddPage()
+
+	var d *Destination
+
+	d, _ = page.CreateDestination()
+	err := d.SetFitBV(0)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+}
