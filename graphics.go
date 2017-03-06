6d866cb9f26f7673ccda8d7877d1fa4785696aef
Brent Rowland <brent.rowland@ttiltd.com>
2017-03-05 16:13:12 -0800

    Wrap HPDF_Page_CurveTo, HPDF_Page_Fill,HPDF_Page_FillStroke, HPDF_Page_LineTo, HPDF_Page_MoveTo, HPDF_Page_Rectangle, HPDF_Page_SetLineWidth and HPDF_Page_Stroke.

--- graphics.go
+++ graphics.go
@@ -6,11 +6,49 @@ package hpdf
 */
 import "C"
 
+func (page *Page) CurveTo(x1 float32, y1 float32, x2 float32, y2 float32, x3 float32, y3 float32) error {
+	C.HPDF_Page_CurveTo(page.page,
+		C.HPDF_REAL(x1), C.HPDF_REAL(y1), C.HPDF_REAL(x2), C.HPDF_REAL(y2), C.HPDF_REAL(x3), C.HPDF_REAL(y3))
+
+	return page.pdf.GetLastError()
+}
+
 func (page *Page) DrawImage(image *Image, x, y, width, height float32) error {
 	C.HPDF_Page_DrawImage(
 		page.page, image.image,
 		C.HPDF_REAL(x), C.HPDF_REAL(y), C.HPDF_REAL(width), C.HPDF_REAL(height),
 	)
+
+	return page.pdf.GetLastError()
+}
+
+func (page *Page) Fill() error {
+	C.HPDF_Page_Fill(page.page)
+
+	return page.pdf.GetLastError()
+}
+
+func (page *Page) FillStroke() error {
+	C.HPDF_Page_FillStroke(page.page)
+
+	return page.pdf.GetLastError()
+}
+
+func (page *Page) LineTo(x float32, y float32) error {
+	C.HPDF_Page_LineTo(page.page, C.HPDF_REAL(x), C.HPDF_REAL(y))
+
+	return page.pdf.GetLastError()
+}
+
+func (page *Page) MoveTo(x float32, y float32) error {
+	C.HPDF_Page_MoveTo(page.page, C.HPDF_REAL(x), C.HPDF_REAL(y))
+
+	return page.pdf.GetLastError()
+}
+
+func (page *Page) Rectangle(x float32, y float32, width float32, height float32) error {
+	C.HPDF_Page_Rectangle(page.page, C.HPDF_REAL(x), C.HPDF_REAL(y), C.HPDF_REAL(width), C.HPDF_REAL(height))
+
 	return page.pdf.GetLastError()
 }
 
@@ -18,6 +56,12 @@ func (page *Page) SetFontAndSize(font *Font, size float32) error {
 	C.HPDF_Page_SetFontAndSize(
 		page.page, font.font, C.HPDF_REAL(size),
 	)
+	return page.pdf.GetLastError()
+}
+
+func (page *Page) SetLineWidth(lineWidth float32) error {
+	C.HPDF_Page_SetLineWidth(page.page, C.HPDF_REAL(lineWidth))
+
 	return page.pdf.GetLastError()
 }
 
@@ -63,6 +107,12 @@ func (page *Page) SetCMYKStroke(c, m, y, k float32) error {
 		C.HPDF_REAL(y),
 		C.HPDF_REAL(k),
 	)
+
+	return page.pdf.GetLastError()
+}
+
+func (page *Page) Stroke() error {
+	C.HPDF_Page_Stroke(page.page)
 
 	return page.pdf.GetLastError()
 }
--- graphics_test.go
+++ graphics_test.go
@@ -4,6 +4,18 @@ import (
 	. "testing"
 )
 
+func TestPageCurveTo(t *T) {
+	pdf, _ := New()
+	page, _ := pdf.AddPage()
+
+	page.MoveTo(0.5, 0.6)
+
+	err := page.CurveTo(1.1, 2.2, 3.3, 4.4, 5.5, 6.6)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
 func TestPageDrawImage(t *T) {
 	pdf, _ := New()
 	page, _ := pdf.AddPage()
@@ -16,6 +28,59 @@ func TestPageDrawImage(t *T) {
 	}
 }
 
+func TestPageFill(t *T) {
+	pdf, _ := New()
+	page, _ := pdf.AddPage()
+
+	page.Rectangle(1.5, 2.5, 3.5, 4.5)
+	err := page.Fill()
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestPageFillStroke(t *T) {
+	pdf, _ := New()
+	page, _ := pdf.AddPage()
+
+	page.Rectangle(1.5, 2.5, 3.5, 4.5)
+	err := page.FillStroke()
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestPageLineTo(t *T) {
+	pdf, _ := New()
+	page, _ := pdf.AddPage()
+
+	page.MoveTo(1.5, 2.5)
+	err := page.LineTo(3.5, 4.5)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestPageMoveTo(t *T) {
+	pdf, _ := New()
+	page, _ := pdf.AddPage()
+
+	err := page.MoveTo(0.5, 0.6)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestPageRectangle(t *T) {
+	pdf, _ := New()
+	page, _ := pdf.AddPage()
+
+	err := page.Rectangle(1.5, 2.5, 3.5, 4.5)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
 func TestPageSetFontAndSize(t *T) {
 	pdf, _ := New()
 	font, _ := pdf.GetFont("Courier")
@@ -23,6 +88,16 @@ func TestPageSetFontAndSize(t *T) {
 
 	err := page.SetFontAndSize(font, 12)
 
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestPageSetLineWidth(t *T) {
+	pdf, _ := New()
+	page, _ := pdf.AddPage()
+
+	err := page.SetLineWidth(2.5)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -63,6 +138,18 @@ func TestPageSetCMYKStroke(t *T) {
 	page, _ := pdf.AddPage()
 
 	err := page.SetCMYKStroke(0.50, 0.32, 0.75, 0.25)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestPageStroke(t *T) {
+	pdf, _ := New()
+	page, _ := pdf.AddPage()
+
+	page.MoveTo(1.5, 2.5)
+	page.LineTo(3.5, 4.5)
+	err := page.Stroke()
 	if err != nil {
 		t.Fatal(err)
 	}
